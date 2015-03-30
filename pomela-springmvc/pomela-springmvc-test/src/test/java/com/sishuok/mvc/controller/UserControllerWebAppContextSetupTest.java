06bd6eb619bca6d45f0c5f4b815bac22b9283edf
tao.he <tao.he@travelzen.com>
2015-03-30 16:38:11 +0800

    整合其它项目

--- pomela-springmvc/pomela-springmvc-test/src/test/java/com/sishuok/mvc/controller/UserControllerWebAppContextSetupTest.java
+++ pomela-springmvc/pomela-springmvc-test/src/test/java/com/sishuok/mvc/controller/UserControllerWebAppContextSetupTest.java
@@ -25,10 +25,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 //XML风格
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration(value = "src/main/webapp")
-@ContextHierarchy({
+@WebAppConfiguration(value = "src/main/webapp")  //用来表示测试环境使用的ApplicationContext将是WebApplicationContext类型的,value指定web应用的根
+@ContextHierarchy({  //指定容器层次，即spring-config.xml是父容器，而spring-mvc.xml是子容器
         @ContextConfiguration(name = "parent", locations = "classpath:spring-config.xml"),
-        @ContextConfiguration(name = "child", locations = "classpath:spring-mvc.xml")
+        @ContextConfiguration(name = "child", locations = "classpath:n_spring-mvc.xml")
 })
 
 //注解风格
@@ -41,20 +41,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class UserControllerWebAppContextSetupTest {
 
     @Autowired
-    private WebApplicationContext wac;
+    private WebApplicationContext wac; //注入web环境的ApplicationContext容器
     private MockMvc mockMvc;
 
     @Before
     public void setUp() {
+        //通过MockMvcBuilders.webAppContextSetup(wac).build()创建一个MockMvc进行测试
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
 
     @Test
     public void testView() throws Exception {
-        mockMvc
-                .perform(get("/user/1"))
+        mockMvc.perform(get("/user/1"))
                 .andExpect(view().name("user/view"))
-                .andExpect(forwardedUrl("/WEB-INF/jsp/user/view.jsp"))
+                .andExpect(forwardedUrl("/WEB-INF/opPage/user/view.jsp"))
                 .andExpect(model().attributeExists("user"))
                 .andExpect(content().string(Matchers.contains("你好")))
                 .andExpect(status().isOk())
