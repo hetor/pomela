3077f271b92910ba02fae5633d805bbd913d0baa
hzhetao <tao.he1989@gmail.com>
2015-09-29 19:16:30 +0800

    add guava, 常量类最佳实践

---
+++ pomela-java/pomela-java-common/src/main/java/pomela/java/common/constants/Constants.java
@@ -0,0 +1,14 @@
+package pomela.java.common.constants;
+
+/**
+ * Created by tao.he on 2015/9/28.
+ *
+ * 最佳实践：final的class，pricate的Constants，public static final的常量，使用时使用静态import常量
+ */
+public final class Constants {
+
+	private Constants() {}
+
+	public static final String NAME = "HETAO";
+	public static final int MAX_NUM = 10;
+}
---
+++ pomela-java/pomela-java-common/src/main/java/pomela/java/common/constants/IConstants.java
@@ -0,0 +1,12 @@
+package pomela.java.common.constants;
+
+/**
+ * Created by tao.he on 2015/9/28.
+ *
+ * 优势：接口类更加轻，简单
+ * 缺点：接口可以被误实现
+ */
+public interface IConstants {
+	String NAME = "HETAO";
+	int MAX_NUM = 10;
+}
