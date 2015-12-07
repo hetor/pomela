c7bd53e900c1ed0d79386fc617050b9eef11482d
tao.he <tao.he1989@gmail.com>
2015-12-07 23:09:58 +0800

    auto_ops.sh

--- scripts/netease/auto_ops.sh
+++ scripts/netease/auto_ops.sh
@@ -36,25 +36,15 @@ function doRefund() {
     echo $ORDER_OUT_ID
 
 	mysql -uhaitao_read -pOfX5LtGcI3Yad1 -h10.120.154.154 -P6000 haitao-mirror <<-EOF 1>result.t 2>err.t
-	    select id,out_id,user_id,user_name,create_time,update_time,deliver_time,pay_time,succ_time,close_time,pre_expire_time,\
-	    origin_goods_total_amount,origin_pay_total_amount,pay_total_amount,tax_amount,coupon_amount,other_service_fee,\
-	    express_china_fee,express_oversea_fee,cost_amount,state,trade_id,logistic_info_id,deliver_date_type,deliver_expect_time,\
-	    customs_place,db_update_time,international_cost_amount,transit_cost_amount,commission_cost_amount,pay_method,hqg_extra_fee,\
-	    real_pay_method_order_id from orders where out_id = '$ORDER_OUT_ID';
+        select state,trade_id,pay_method,hqg_extra_fee,real_pay_method_order_id,from_unixtime(deliver_time) as deliver_time,from_unixtime(pay_time) as pay_time,from_unixtime(succ_time) as succ_time,from_unixtime(close_time) as close_time from orders where out_id = '$ORDER_OUT_ID';
 	
-	    select id,out_id,user_id,user_name,trade_id,trade_out_id,order_id,order_out_id,order_item_id,order_item_out_id,create_time,\
-	    update_time,transfer_time,amount,state,pay_method,account_out,account_in,note,type,from_type,sku_data,db_update_time,\
-	    account_in_name from trade_refund_transfer where order_out_id='$ORDER_OUT_ID';
+            select out_id,order_out_id,order_item_out_id,from_unixtime(create_time) as create_time,from_unixtime(transfer_time) as transfer_time,amount,state,pay_method,account_out,account_in,type,from_type
+	    from trade_refund_transfer where order_out_id='$ORDER_OUT_ID';
 	
-	    select id,order_id,gorder_id,user_name,user_id,create_time,update_time,close_time,pay_time,pre_expire_time,origin_total_amount,\
-	    origin_pay_total_amount,pay_total_amount,coupon_amount,activity_amount,tax_amount,express_oversea_fee,express_china_fee,\
-	    other_service_fee,cost_amount,coupon_id,state,real_pay_method,pay_method,invoice,wyb_order_id,description,hand_fee,entry_from,\
-	    real_pay_method_order_id,pay_method_sub_type,db_update_time,owner_type,real_pay_method_account,is_merge_payment from trade where order_id = '$ORDER_OUT_ID';
+        select gorder_id,from_unixtime(create_time) as create_time,from_unixtime(update_time) as update_time,from_unixtime(close_time) as close_time,from_unixtime(pay_time) as pay_time,state,real_pay_method,pay_method,wyb_order_id,real_pay_method_order_id,pay_method_sub_type,owner_type,real_pay_method_account,is_merge_payment from trade where order_id = '$ORDER_OUT_ID';
 
 	    select * from retry_task where search_key = '$ORDER_OUT_ID';
 	EOF
-
-    printf "%10s %10s %10s %10s %10s %10s %10s",  "order_id", "transfer_time", "ÍË¿î×´Ì¬", "pay_method", "real_pay_method_order_id", "is_merge_payment", "pay_method_sub_type", "¶©µ¥×´Ì¬"
 
     sed 's/\s/|/g' result.t | sed 's/|/ /g' > result.t
 
