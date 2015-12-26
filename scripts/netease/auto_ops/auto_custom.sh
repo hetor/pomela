de9ea1c24a3d660a57811a6c1dd14ec1c2442a91
hzhetao <tao.he1989@gmail.com>
2015-12-26 11:15:20 +0800

    auto_custom.sh

--- scripts/netease/auto_ops/auto_custom.sh
+++ scripts/netease/auto_ops/auto_custom.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 export LANG=zh_CN.UTF-8
 
+CURRENT_USER="tao.he"
+IP="106.2.33.38"
+ENV="pre3"
+TOMCAT_PATH="tomcat-haitao-pay-Ins1"
+
+PAY_PORT="8191"
+HAITAO_PORT="8182"
 
 function custom_file() {
     IP="106.2.33.38"
@@ -17,30 +24,32 @@ function custom_file() {
         echo "http://$IP:$APP/manualinvoke/acquireCustoms?orderId=$orderId"
     done
 
-	ssh $CURRENT_USER@$IP -p 1046 <<-EOF
-	    cd /home/appops/haitaoTest/pre3/tomcat-haitao-pay-Ins1/logs
-	    tail -$ORDER_NUM PayLog.log
+	ssh $CURRENT_USER@$IP -p 1046 <<-EOF cd /home/appops/haitaoTest/pre3/tomcat-haitao-pay-Ins1/logs tail -$ORDER_NUM PayLog.log
 	EOF
 }
 
 function custom_order_ids() {
-    IP="106.2.33.38"
-    [ "$1" == "pay" ] && APP="8191" || APP="8182"
+	ORDER_OUT_ID="${1}"
 
-    grep -e '[0-9]\{25\}' orderIds.txt | sed 's/[^0-9]/|/g' | sed 's/|[0-9]\{1,24\}|/|/g' | sed 's/|[0-9]\{26,\}|/|/g' \
-        | sed 's/|/\n/g' | grep -v '^$' | sort | uniq > orderIds.tmp
+	mysql -uhaitao_read -pOfX5LtGcI3Yad1 -h 10.120.154.154 -P 6000 haitao-mirror <<-EOF 1>result.tmp 2>err.tmp
+		select is_merge_payment from trade where order_id = '$ORDER_OUT_ID';
+	EOF
 
-    ORDER_NUM=`grep -v '^$' orderIds.tmp | wc -l`
-
-    for orderId in `cat orderIds.tmp`
-    do
-        #curl "http://$IP:$APP/manualinvoke/acquireCustoms?orderId=$orderId"
-        echo "http://$IP:$APP/manualinvoke/acquireCustoms?orderId=$orderId"
-    done
+	IS_MERGE_PAYMENT=`cat result.tmp | awk 'NR==2{print $1}'`
+	
+	if [ "${IS_MERGE_PAYMENT}" == "0" ]; then
+		TOMCAT_PATH="tomcat-haitao-Ins1"
+		echo "http://$IP:$HAITAO_PORT/manualinvoke/acquireCustoms?orderId=$ORDER_OUT_ID"
+		curl "http://$IP:$HAITAO_PORT/manualinvoke/acquireCustoms?orderId=$ORDER_OUT_ID"
+	else
+		TOMCAT_PATH="tomcat-haitao-pay-Ins1"
+		echo "http://$IP:$PAY_PORT/manualinvoke/acquireCustoms?orderId=$ORDER_OUT_ID"
+		curl "http://$IP:$PAY_PORT/manualinvoke/acquireCustoms?orderId=$ORDER_OUT_ID"
+	fi
 
 	ssh $CURRENT_USER@$IP -p 1046 <<-EOF
-	    cd /home/appops/haitaoTest/pre3/tomcat-haitao-pay-Ins1/logs
-	    tail -$ORDER_NUM PayLog.log
+		cd /home/appops/haitaoTest/${ENV}/${TOMCAT_PATH}/logs
+		cat PayLog.log | grep --color=auto '$ORDER_OUT_ID'
 	EOF
 }
 
@@ -48,13 +57,14 @@ function custom_order_ids() {
 
 case ${1} in
     "-f")
-        custom_file "${2}"
-        shift 2
-        ;;
+	    custom_file "${2}" 
+	    #shift 2
+	    ;;
     "-o")
-        custom_order_ids "${2}"
-        shift 2
+	    custom_order_ids "${2}"
+	    #shift 2
+	    ;;
     *)
-        echo "Usage {-f filename | -o orderIds}"
-        ;;
+	    echo "Usage {-f filename | -o orderIds}"
+	    ;;
 esac
--- scripts/netease/auto_ops/auto_pros.sh
+++ scripts/netease/auto_ops/auto_pros.sh
@@ -21,31 +21,31 @@ HAITAO_HOME="/c/Users/tao.he/Code/haitao"
 #
 #}
 
-#function pros_add() {
-#    PROJECT_HOME=$1
-#    for file in $(find $PROJECT_HOME/ -name 'pay.properties')
-#    do
-#        echo "File: ${file}"
-#        sed -i 's/alipay.transfer.callback.url/alipay.transfer.mergepay.callback.url/g' ${file}
-#    done
-#
-#}
-
 function pros_add() {
     PROJECT_HOME=$1
     for file in $(find $PROJECT_HOME/ -name 'pay.properties')
     do
         echo "File: ${file}"
-        #echo `grep 'wyb.alipay.account=neteasehkhqg@service.netease.com' ${file}`
-        #sed -i -n '/^wyb\.alipay\.account/d' ${file}
-        sed -i '/2088101153102215/a\
-\
-alipay.domestic.hangzhou.youmai.pf.name.str=\\u676d\\u5dde\\u4f18\\u4e70\\u79d1\\u6280\\u6709\\u9650\\u516c\\u53f8\
-alipay.domestic.hangzhou.youmai.pf.account=neteaseyoumaipf@service.netease.com\
-alipay.domestic.hangzhou.youmai.pf.account.id=2088121421423585' ${file}
+        sed -i 's/wyb.identify.pc.quick.identify=http/wyb.identify.pc.quick.identify=https/g' ${file}
     done
 
 }
+
+#function pros_add() {
+#    PROJECT_HOME=$1
+#    for file in $(find $PROJECT_HOME/ -name 'pay.properties')
+#    do
+#        echo "File: ${file}"
+#        #echo `grep 'wyb.alipay.account=neteasehkhqg@service.netease.com' ${file}`
+#        #sed -i -n '/^wyb\.alipay\.account/d' ${file}
+#        sed -i '/2088101153102215/a\
+#\
+#alipay.domestic.hangzhou.youmai.pf.name.str=\\u676d\\u5dde\\u4f18\\u4e70\\u79d1\\u6280\\u6709\\u9650\\u516c\\u53f8\
+#alipay.domestic.hangzhou.youmai.pf.account=neteaseyoumaipf@service.netease.com\
+#alipay.domestic.hangzhou.youmai.pf.account.id=2088121421423585' ${file}
+#    done
+#
+#}
 
 case ${1} in
     "dw")
