e1d0d3aae313cfe994132a66568c03e4cc681a4d
tao.he <tao.he1989@gmail.com>
2016-10-11 09:57:21 +0800

    add ExtSerializerFactory Demo

--- pomela-java/pomela-java-serialize/src/main/java/pomela/java/serialize/hessian/ExtSerializerDemo.java
+++ pomela-java/pomela-java-serialize/src/main/java/pomela/java/serialize/hessian/ExtSerializerDemo.java
@@ -1,14 +1,86 @@
 package pomela.java.serialize.hessian;
 
+import com.caucho.hessian.io.*;
+
+import java.io.IOException;
+import java.io.Serializable;
+
 /**
  * Created by hetor on 2016/10/10.
  */
 public class ExtSerializerDemo {
 	public static void main(String[] args) {
 		ExtSerializerFactory extSerializerFactory = new ExtSerializerFactory();
-		extSerializerFactory.addSerializer(class , mySerializer); //添加自定义的序列化接口
-		extSerializerFactory.addDeserializer(class , myDeserializer); //添加自定义的反序列化接口
+		extSerializerFactory.addSerializer(Req.class, new MySerializer()); //添加自定义的序列化接口
+		extSerializerFactory.addDeserializer(Req.class, new MyDeserializer()); //添加自定义的反序列化接口
 
+		SerializerFactory serializerFactory = null; //TODO get the serializerFactory
 		serializerFactory.addFactory(extSerializerFactory); //注册ext到序列化工厂
+	}
+
+
+	static class Req implements Serializable {
+
+	}
+
+	static class MySerializer implements Serializer {
+
+		@Override
+		public void writeObject(Object o, AbstractHessianOutput abstractHessianOutput) throws IOException {
+
+		}
+	}
+
+	static class MyDeserializer implements Deserializer {
+
+		@Override
+		public Class<?> getType() {
+			return null;
+		}
+
+		@Override
+		public boolean isReadResolve() {
+			return false;
+		}
+
+		@Override
+		public Object readObject(AbstractHessianInput abstractHessianInput) throws IOException {
+			return null;
+		}
+
+		@Override
+		public Object readList(AbstractHessianInput abstractHessianInput, int i) throws IOException {
+			return null;
+		}
+
+		@Override
+		public Object readLengthList(AbstractHessianInput abstractHessianInput, int i) throws IOException {
+			return null;
+		}
+
+		@Override
+		public Object readMap(AbstractHessianInput abstractHessianInput) throws IOException {
+			return null;
+		}
+
+		@Override
+		public Object[] createFields(int i) {
+			return new Object[0];
+		}
+
+		@Override
+		public Object createField(String s) {
+			return null;
+		}
+
+		@Override
+		public Object readObject(AbstractHessianInput abstractHessianInput, Object[] objects) throws IOException {
+			return null;
+		}
+
+		@Override
+		public Object readObject(AbstractHessianInput abstractHessianInput, String[] strings) throws IOException {
+			return null;
+		}
 	}
 }
